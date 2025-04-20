0cfaebd9c58c6f3084298d461c033c6e026112bf
Tenshix6 <152530031+Tenshix6@users.noreply.github.com>
2025-04-20 06:20:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
+const { spawn } = require('child_process');
 const app = express();
 
+app.get('/stream', (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.status(400).send('Missing URL');
 
-app.get('/music', (req, res) => {
-  const youtubeUrl = req.query.url;
+  const ytdlp = spawn('yt-dlp', ['-f', 'bestaudio', '-o', '-', url]);
 
-  res.header("Content-Type", "audio/mpeg");
+  res.setHeader('Content-Type', 'audio/mpeg');
 
- 
-  ytdl(youtubeUrl, { filter: 'audioonly' }).pipe(res);
+  ytdlp.stdout.pipe(res);
+
+  ytdlp.stderr.on('data', (data) => {
+    console.error(`yt-dlp error: ${data}`);
+  });
+
+  ytdlp.on('close', (code) => {
+    console.log(`yt-dlp exited with code ${code}`);
+    res.end();
+  });
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server running on port 3000');
 });
